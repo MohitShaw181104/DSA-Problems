948106b09a737bda7a12a0689f459b05953e80f2
Mohit <mohitshaw181104@gmail.com>
2024-01-03 21:02:02 +0530

    minute

--- Part_1/Basic/Sum Of Max And Min.cpp
+++ Part_1/Basic/Sum Of Max And Min.cpp
@@ -1,4 +1,4 @@
-/*You are given an array “ARR” of size N. Your task is to find out the sum of maximum and minimum elements in the array.*/
+/*You are given an array “ARR” of size N. Your task is to find out the sum of the maximum and minimum elements in the array.*/
 
 #include <bits/stdc++.h> 
 int sumOfMaxMin(int arr[], int n){
