6aaeae00e7508c91bbb6d96fca7d6970d3bad88f
Mohit <mohitshaw181104@gmail.com>
2024-06-11 12:10:18 +0530

    p4

---
+++ Strivera-z dsa sheet/patterns/p4.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void p4(int n){
+    for (int i = 1; i <=n ; i++)
+    {
+        /* code */for (int j = 1; j<=i ; j++)
+        {
+            /* code */cout<< i << "";
+
+        }
+        cout<<endl;        
+    }
+    
+}
+
+int main(){
+    int n=5;
+
+    p4(n);
+    return 0;
+}
