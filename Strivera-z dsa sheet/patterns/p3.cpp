74bf20848546fbcaae5ba8a7551e596c4a12f17c
Mohit <mohitshaw181104@gmail.com>
2024-06-11 12:03:27 +0530

    3rd

--- Strivera-z dsa sheet/patterns/p3.cpp
+++ Strivera-z dsa sheet/patterns/p3.cpp
@@ -1,2 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+void p3(int n){
+    for (int i = 0; i <= n; i++)
+    {
+        /* code */for (int j = 1; j <=i ; j++)
+        {
+            /* code */cout << j << "";
+        }
+        cout << endl;
+        
+    }
+    
+}
+
+int main(){
+    int n=5;
+
+    p3(n);
+    return 0;
+
+}
